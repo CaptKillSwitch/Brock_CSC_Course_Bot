e72782560e110bca257527d196e137dead24f415
CaptKillSwitch <62815232+CaptKillSwitch@users.noreply.github.com>
2022-05-28 21:23:40 -0400

    Updated the you are not allowed command to be only visibile to the user

--- index.js
+++ index.js
@@ -479,7 +479,7 @@ async function start() {
                 } else {
                   await interaction.reply({
                     content: "You are not allowed to use this command.",
-                    ephemeral: false,
+                    ephemeral: true,
                   });
                 }
                 return;
@@ -580,7 +580,7 @@ async function start() {
                 } else {
                   await interaction.reply({
                     content: "You are not allowed to use this command.",
-                    ephemeral: false,
+                    ephemeral: true,
                   });
                 }
                 return;
@@ -650,7 +650,7 @@ async function start() {
                   } else {
                     await interaction.reply({
                       content: "You are not allowed to use this command.",
-                      ephemeral: false,
+                      ephemeral: true,
                     });
                   }
                 } catch (e) {
@@ -727,7 +727,7 @@ async function start() {
                   } else {
                     await interaction.reply({
                       content: "You are not allowed to use this command.",
-                      ephemeral: false,
+                      ephemeral: true,
                     });
                   }
                 } catch (e) {
@@ -817,7 +817,7 @@ async function start() {
                   } else {
                     await interaction.reply({
                       content: "You are not allowed to use this command.",
-                      ephemeral: false,
+                      ephemeral: true,
                     });
                   }
                 } catch (e) {
